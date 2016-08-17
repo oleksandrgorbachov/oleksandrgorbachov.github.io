07d7e3897fe52f75a3c9f888e9a31a142d138af2
oleksandrgorbachov <gorbachov@live.ru>
2016-08-17 21:02:29 +0300

    Update script.js

--- js1-2/js/script.js
+++ js1-2/js/script.js
@@ -1,26 +1,21 @@
-    
+var names = [];
+ 
 for (var i = 0; i < 5; i++) {
-    var names = [];
     names [ i ] = prompt ('Введите имя ');
 }
 
 var user = prompt('Введите имя пользователя', '');
-	
-if (user.length === 0) {
-	alert('Вы не ввели имя пользователя');
-	user = prompt('Введите имя пользователя', '');
-}
 
 var userNames = 0;
 
-for (i = 1; i < 2; i++) {
-    if (user == names[i]) {
+for (var i = 1; i < 5; i++) {
+    if (names[i] == user) {
         userNames = 1;
         break;
     }   
 }
 
-if ( userNames == 1) {
+if ( userNames ) {
    alert(user + ', вы успешно вошли!');
 } else {
     alert('В списке нет пользователя с таким именем');
